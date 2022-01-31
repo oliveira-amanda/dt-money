ada4519e2abd4daa171b2595b12bc4b01e12f27f
oliveira-amanda <amanda.bap.oliveira@gmail.com>
2022-01-31 17:32:10 -0300

    Creating fake request with miragejs

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard () {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <Container>
       <Summary />
