0216a0bc0f2fcdbbf2a26c572b404af633da5b09
oliveira-amanda <amanda.bap.oliveira@gmail.com>
2022-02-07 21:35:17 -0300

    Add seeds, add transactions in state, list transactions

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,15 +1,8 @@
-import { useEffect } from "react";
-import { api } from "../../services/api";
 import { Summary } from "../Summary";
 import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard () {
-  useEffect(() => {
-    api.get('transactions')
-      .then(data => console.log(data))
-  }, []);
-
   return (
     <Container>
       <Summary />
